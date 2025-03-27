64c521373386b8c0ad0d65e6a01f890b09333ee1
Ajinkya_Ranshur <ranshur.ajinkya.res@gmail.com>
2025-03-28 05:16:38 +0530

    Stacks code updated

--- Assignments/Q16mod.cpp
+++ Assignments/Q16mod.cpp
@@ -7,7 +7,6 @@ using namespace std;
 template <typename T>
 class Stack{
 
-
 private:
 	T* arr;
 	int top;
@@ -56,22 +55,78 @@ public:
 		return top==0 && arr[top]==0;
 	}
 
-
 	
 };
 
+template <typename T>
+class qfromstacks{
+
+private:
+	Stack<T> s1,s2;
+
+public:
+
+    qfromstacks(int size) :	s1(size),s2(size) {}
+	
+    void enqueue(T x) {
+        s1.push(x);
+    }
+
+    T dequeue() {
+        if (s2.is_empty()) {
+            while (!s1.is_empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        if (!s2.is_empty()) {
+            return s2.pop();
+        } else {
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }
+    }
+
+    T front() {
+        if (s2.is_empty()) {
+            while (!s1.is_empty()) {
+                s2.push(s1.pop());
+            }
+        }
+        if (!s2.is_empty()) {
+            return s2.topelem();
+        } else {
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }
+    }
+
+    int size() {
+        return s1.size() + s2.size();
+    }
+
+
+
+};
+
+
 int main() {
 
-Stack<int> s(5);
-
-s.push(10);
-s.push(20);
-s.push(30);
-s.push(40);
+qfromstacks<int> q(5);
 
 
-cout <<"Size of the stack"<< s.size()<<endl;
-cout <<"Top Element"<<s.topelem() <<endl;
+q.enqueue(10);
+q.enqueue(20);
+q.enqueue(30);
+q.enqueue(40);
+
+cout << "Front Element: " << q.front() << endl;
+cout << "Dequeued: " << q.dequeue() << endl;
+cout << "Dequeued: " << q.dequeue() << endl;
+
+q.enqueue(50);
+
+cout << "Front Element: " << q.front() << endl;
+cout << "Queue Size: " << q.size() << endl;
 
 }
 
